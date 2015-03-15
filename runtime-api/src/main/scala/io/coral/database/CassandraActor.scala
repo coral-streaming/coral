28c97050365ebae80d19e889472400bd95d3fc67
Natalino Busa <natalino.busa@gmail.com>
2015-03-15 16:30:11 +0100

    replaced 4 spaces indents by 2 spaces: http://docs.scala-lang.org/style/indentation.html

--- runtime-api/src/main/scala/io/coral/database/CassandraActor.scala
+++ runtime-api/src/main/scala/io/coral/database/CassandraActor.scala
@@ -19,130 +19,124 @@ import scalaz.{OptionT, Monad}
 import scalaz.OptionT._
 
 object CassandraActor {
-    implicit val formats = org.json4s.DefaultFormats
+  implicit val formats = org.json4s.DefaultFormats
 
-    def getParams(json: JValue) = {
-        for {
-            host <- (json \ "host").extractOpt[String]
-            keyspace <- (json \ "keyspace").extractOpt[String]
-        } yield {
-            (host, keyspace)
-        }
+  def getParams(json: JValue) = {
+    for {
+      host <- (json \ "host").extractOpt[String]
+      keyspace <- (json \ "keyspace").extractOpt[String]
+    } yield {
+      (host, keyspace)
     }
+  }
 
-    def apply(json: JValue): Option[Props] = {
-        getParams(json).map(_ => Props(classOf[CassandraActor], json))
-    }
+  def apply(json: JValue): Option[Props] = {
+    getParams(json).map(_ => Props(classOf[CassandraActor], json))
+  }
 }
 
 class CassandraActor(json: JObject) extends CoralActor {
-    def jsonDef = json
+  def jsonDef = json
 
-    val (host, keyspace) = CassandraActor.getParams(json).get
+  val (host, keyspace) = CassandraActor.getParams(json).get
 
-    override def preStart() {
-        ensureConnection(host, keyspace)
-    }
+  override def preStart() {
+    ensureConnection(host, keyspace)
+  }
 
-    var cluster: Cluster = _
-    var session: Session = _
-    var schema: JObject = _
-    var tables = List.empty[String]
+  var cluster: Cluster = _
+  var session: Session = _
+  var schema: JObject = _
+  var tables = List.empty[String]
 
-    def state = Map(
-        ("connected", render(session != null && !session.isClosed)),
-        ("keyspace", render(keyspace)),
-        ("schema", render(schema)),
-        ("tables", render(tables))
-    )
+  def state = Map(
+    ("connected", render(session != null && !session.isClosed)),
+    ("keyspace",  render(keyspace)),
+    ("schema",  render(schema)),
+    ("tables",  render(tables))
+  )
 
-    def timer = JNothing
+  def timer = JNothing
 
-    // Stores the intermediate result for emit.
-    // A select returns a resultset, any other
-    // query type (update, insert, delete, ...) returns
-    // a boolean indicating success or failure
-    var result: Option[Either[ResultSet, Boolean]] = _
+  // Stores the intermediate result for emit.
+  // A select returns a resultset, any other
+  // query type (update, insert, delete, ...) returns
+  // a boolean indicating success or failure
+  var result: Option[Either[ResultSet, Boolean]] = _
 
-    def trigger = {
-        json: JObject =>
-            ensureConnection(host, keyspace)
+  def trigger = {
+    json: JObject =>
+      ensureConnection(host, keyspace)
 
-            // Add this when you do not want a for comprehension:
-            // OptionT.some(Future.successful({}))
+      // Add this when you do not want a for comprehension:
+      // OptionT.some(Future.successful({}))
 
-            for {
-                query <- getTriggerInputField[String](json \ "query")
-                data <- OptionT.some(session.execute(query))
-            } yield {
-                // TODO: How to catch exception and return Right(false)
-                if (query.startsWith("select")) {
-                    Some(Left(data))
-                } else {
-                    Some(Right(true))
-                }
-            }
-    }
-
-    def emit = {
-        json: JObject =>
-            result match {
-                case Some(Left(data)) =>
-                    renderResultSet(data)
-                case Some(Right(successful)) =>
-                    render(("result" -> successful))
-                case None =>
-                    JNothing
-            }
-    }
-
-    def ensureConnection(host: String, keyspace: String) {
-        // No need to connect if already having a valid session object
-        if (session == null || session.isClosed) {
-            log.info("CassandraActor not yet connected. Connecting now...")
-            cluster = Cluster.builder().addContactPoint(host).build()
-            session = cluster.connect(keyspace)
-            schema = getSchema(session)
-            tables = getTables(session)
+      for {
+        query <- getTriggerInputField[String](json \ "query")
+        data <- OptionT.some(session.execute(query))
+      } yield {
+        // TODO: How to catch exception and return Right(false)
+        if (query.startsWith("select")) {
+          Some(Left(data))
         } else {
-            log.info("CassandraActor already connected.")
+          Some(Right(true))
         }
+      }
+  }
+
+  def emit = {
+    json: JObject =>
+      result match {
+        case Some(Left(data)) =>
+          renderResultSet(data)
+        case Some(Right(successful)) =>
+          render(("result" -> successful))
+        case None =>
+          JNothing
+      }
+  }
+
+  def ensureConnection(host: String, keyspace: String) {
+    // No need to connect if already having a valid session object
+    if (session == null || session.isClosed) {
+      log.info("CassandraActor not yet connected. Connecting now...")
+      cluster = Cluster.builder().addContactPoint(host).build()
+      session = cluster.connect(keyspace)
+      schema = getSchema(session)
+      tables = getTables(session)
+    } else {
+      log.info("CassandraActor already connected.")
+    }
+  }
+
+  def getTables(session: Session): List[String] = {
+    val result = scala.collection.mutable.ListBuffer.empty[String]
+
+    val tablemetadata = session.getCluster.getMetadata
+      .getKeyspace(keyspace).getTables.iterator()
+
+    while (tablemetadata.hasNext) {
+      result += tablemetadata.next().getName
     }
 
-    def getTables(session: Session): List[String] = {
-        val result = scala.collection.mutable.ListBuffer.empty[String]
+    result.toList
+  }
 
-        val tablemetadata = session.getCluster.getMetadata
-            .getKeyspace(keyspace).getTables.iterator()
+  def getSchema(session: Session): JObject = {
+    val result = session.getCluster.getMetadata.exportSchemaAsString()
+    render(result).asInstanceOf[JObject]
+  }
+  
+  // todo: ???
+  def renderResultSet(data: ResultSet): JObject = {
+    val it = data.iterator()
+    val columns = data.getColumnDefinitions
 
-        while (tablemetadata.hasNext) {
-            result += tablemetadata.next().getName
-        }
-
-        result.toList
+    while (it.hasNext) {
+      val row = it.next
     }
 
-    def getSchema(session: Session): JObject = {
-        val result = session.getCluster.getMetadata.exportSchemaAsString()
-        render(result).asInstanceOf[JObject]
-    }
-
-    /*
-    def getTriggerInputField[A](jsonValue: JValue)(implicit mf: Manifest[A]) = {
-        val value = Future.successful(jsonValue.extractOpt[A])
-        optionT(value)
-    }
-    */
-
-    def renderResultSet(data: ResultSet): JObject = {
-        val it = data.iterator()
-        val columns = data.getColumnDefinitions
-
-        while (it.hasNext) {
-            val row = it.next
-        }
-
-        val stringRep = DataType.serializeValue().toString
-        null
-    }
+    val stringRep = DataType.serializeValue toString
+    null
+  }
 }
