40ac00bbd410ceae274515da3dc65ec88199af1c
Natalino Busa <natalino.busa@gmail.com>
2015-03-15 19:58:08 +0100

    Coral Actors must be tested only using their interface / behaviour

--- runtime-api/src/test/scala/io/coral/actors/TestThresholdActor.scala
+++ runtime-api/src/test/scala/io/coral/actors/TestThresholdActor.scala
@@ -1,16 +1,24 @@
 package io.coral.actors
 
-import akka.actor.{Props, ActorSystem}
-import akka.testkit.{TestKit, ImplicitSender}
+// scala
+
+import io.coral.actors.transform.ThresholdActor
+
+import scala.concurrent.duration._
+
+// akka
+import akka.actor.ActorSystem
+import akka.testkit.{TestActors, TestActorRef, TestKit, ImplicitSender}
 import akka.util.Timeout
-import io.coral.actors.Messages.{CreateActor, GetActorPath, RegisterActor, DeleteAllActors}
+import akka.pattern.ask
+
+// json
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
+
+// scalatest
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import akka.pattern.ask
-import akka.actor.ActorPath
+
 
 class TestThresholdActor(_system: ActorSystem) extends TestKit(_system)
   with ImplicitSender
@@ -20,7 +28,6 @@ class TestThresholdActor(_system: ActorSystem) extends TestKit(_system)
 
   implicit val timeout = Timeout(100.millis)
   def this() = this(ActorSystem("TestThreshold"))
-  val runtime = system.actorOf(Props[RuntimeActor], "coral")
 
   override def afterAll() {
     TestKit.shutdownActorSystem(system)
@@ -31,13 +38,14 @@ class TestThresholdActor(_system: ActorSystem) extends TestKit(_system)
       """{ "type": "threshold", "params":
       |{ "key": "key1", "threshold": 10.5 } }"""
       .stripMargin).asInstanceOf[JObject]
-    runtime ! CreateActor(createJson)
-    expectMsg(Some(1))
-    
-    val result = (runtime ? GetActorPath(1)).mapTo[Option[ActorPath]]
-    val threshold = system.actorSelection(Await.result(result, timeout.duration).get)
-    threshold ! RegisterActor(testActor.actorRef)
-    
+
+    // test invalid definition json as well !!!
+    val props = CoralActorFactory.getProps(createJson).get
+    val threshold = TestActorRef[ThresholdActor](props)
+
+    // subscribe echo on the test actor
+    threshold.underlyingActor.emitTargets += testActor
+
     "Emit when equal to the threshold" in {
       val json = parse("""{"key1": 10.5}""").asInstanceOf[JObject]
       threshold ! json
@@ -46,13 +54,13 @@ class TestThresholdActor(_system: ActorSystem) extends TestKit(_system)
     
     "Emit when higher than the threshold" in {
       val json = parse("""{"key1": 10.7}""").asInstanceOf[JObject]
-      threshold ! json
+      threshold ? json
       expectMsg(parse("""{"key1": 10.7, "thresholdReached": "key1"}"""))
     }
 
     "Not emit when lower than the threshold" in {
       val json = parse("""{"key1": 10.4}""").asInstanceOf[JObject]
-      threshold ! json
+      threshold ? json
       expectNoMsg
     }
 
