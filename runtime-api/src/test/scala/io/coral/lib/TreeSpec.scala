c6ec6f09ea405affc057b9e4a01f5db9a13114e3
Hoda <hoda.alemi@gmail.com>
2015-03-24 15:45:20 +0100

    Unit tests are done for binary tree: fixed the bug

--- runtime-api/src/test/scala/io/coral/lib/TreeSpec.scala
+++ runtime-api/src/test/scala/io/coral/lib/TreeSpec.scala
@@ -88,11 +88,6 @@ class TreeSpec extends WordSpecLike with ShouldMatchers {
       tree.postorder should equal(expectedTraverse)
     }
 
-    "" in {
-      val tree: Tree[Int] = new Tree[Int]()
-      tree.add(6); tree.add(3); tree.add(2); tree.add(4); tree.add(8); tree.add(8); tree.add(9)
-
-    }
   }
 
 }
