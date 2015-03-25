b6287d4b6fb90dfec5703893d53d85b110a13715
Hoda <hoda.alemi@gmail.com>
2015-03-25 13:01:53 +0100

    Unit tests are 97% done for the binary tree. (using the coverage tool)

--- runtime-api/src/test/scala/io/coral/lib/TreeSpec.scala
+++ runtime-api/src/test/scala/io/coral/lib/TreeSpec.scala
@@ -19,12 +19,73 @@ class TreeSpec extends WordSpecLike with ShouldMatchers {
       tree should not equal(emptyTree)
     }
 
-    "remove a value from the tree" in {
+    "remove a leaf node from the tree" in {
       val tree: Tree[Int] = new Tree[Int]()
       val anotherTree : Tree[Int] = new Tree[Int]()
       tree.add(1); tree.add(2); tree.add(3)
       anotherTree.add(1); anotherTree.add(2)
       tree.remove(3)
+      tree.size should equal(anotherTree.size)
+    }
+
+    "remove a node with parent" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      val anotherTree : Tree[Int] = new Tree[Int]()
+      tree.add(6); tree.add(3); tree.add(2); tree.add(4); tree.add(8); tree.add(8); tree.add(9)
+      anotherTree.add(6); anotherTree.add(2); anotherTree.add(4); anotherTree.add(8); anotherTree.add(8); anotherTree.add(9)
+      tree.remove(3)
+      tree.size should equal(anotherTree.size)
+    }
+
+    "remove the root of a balanced tree" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      val anotherTree : Tree[Int] = new Tree[Int]()
+      tree.add(6); tree.add(3); tree.add(2); tree.add(4); tree.add(8); tree.add(8); tree.add(9)
+      anotherTree.add(3); anotherTree.add(2); anotherTree.add(4); anotherTree.add(8); anotherTree.add(8); anotherTree.add(9)
+      tree.remove(6)
+      tree.inorder should equal(anotherTree.inorder)
+    }
+
+    "remove a node that has a right child and a parent but not a left child" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      val anotherTree : Tree[Int] = new Tree[Int]()
+      tree.add(1); tree.add(2); tree.add(3)
+      anotherTree.add(1); anotherTree.add(3)
+      tree.remove(2)
+      tree.inorder should equal(anotherTree.inorder)
+    }
+
+    "remove a node that has a left child and a parent but not a right child" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      val anotherTree : Tree[Int] = new Tree[Int]()
+      tree.add(3); tree.add(2); tree.add(1)
+      anotherTree.add(3); anotherTree.add(1)
+      tree.remove(2)
+      tree.inorder should equal(anotherTree.inorder)
+    }
+
+    "remove a value that does not exist in the tree" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      tree.add(3); tree.add(2); tree.add(1)
+      tree.remove(6)
+      tree should equal(tree)
+    }
+
+    "remove the root of a tree that only has left children" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      val anotherTree : Tree[Int] = new Tree[Int]()
+      tree.add(3); tree.add(2);tree.add(1)
+      anotherTree.add(2); anotherTree.add(1)
+      tree.remove(3)
+      tree.size should equal(anotherTree.size)
+    }
+
+    "remove the root of a tree that only has right children" in {
+      val tree: Tree[Int] = new Tree[Int]()
+      val anotherTree : Tree[Int] = new Tree[Int]()
+      tree.add(1); tree.add(2);tree.add(3)
+      anotherTree.add(2); anotherTree.add(3)
+      tree.remove(1)
       tree.size should equal(anotherTree.size)
     }
 
@@ -41,7 +102,7 @@ class TreeSpec extends WordSpecLike with ShouldMatchers {
       tree.height should equal(2)
     }
 
-    "have correct height in the balance tree" in {
+    "have correct height in a balance tree" in {
       val tree: Tree[Int] = new Tree[Int]()
       tree.add(6); tree.add(3); tree.add(2); tree.add(4); tree.add(8); tree.add(8); tree.add(9)
       tree.height should equal(2)
@@ -52,7 +113,7 @@ class TreeSpec extends WordSpecLike with ShouldMatchers {
       tree.size should equal(0)
     }
 
-    "have size 1 for tree with 1 node" in {
+    "have size 1 for a tree with 1 node" in {
       val tree: Tree[Int] = new Tree[Int]()
       tree.add(10)
       tree.size should equal(1)
