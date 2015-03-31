51ea883ad118ab6995a8568c330c5621c08edebb
Koen Verschuren <koenzijnpost@gmail.com>
2015-03-31 09:38:57 +0200

    Changed naming of the httpServer actor to better understandable one. Also setup unit testing

---
+++ runtime-api/src/main/scala/io/coral/actors/transform/HttpBroadcastActor.scala
@@ -0,0 +1,26 @@
+package io.coral.actors.transform
+
+// akka
+import akka.actor.{ActorLogging, Props}
+
+//json goodness
+import org.json4s._
+
+// coral
+import io.coral.actors.CoralActor
+
+object HttpBroadcastActor {
+  implicit val formats = org.json4s.DefaultFormats
+
+  def apply(json: JValue): Option[Props] = {
+    Some(Props(classOf[HttpBroadcastActor], json))
+  }
+}
+
+class HttpBroadcastActor(json: JObject) extends CoralActor with ActorLogging {
+  def jsonDef = json
+  def state = Map.empty
+  def trigger = noProcess
+  def emit = passThroughEmit
+  def timer = notSet
+}
--- runtime-api/src/test/scala/io/coral/actors/transform/HttpBroadcastActorSpec.scala
+++ runtime-api/src/test/scala/io/coral/actors/transform/HttpBroadcastActorSpec.scala
@@ -9,7 +9,7 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 /**
  * Created by us73ut on 3/25/15.
  */
-class HttpServerActorSpec(_system: ActorSystem)
+class HttpBroadcastActorSpec(_system: ActorSystem)
   extends TestKit(_system)
   with ImplicitSender
   with WordSpecLike
