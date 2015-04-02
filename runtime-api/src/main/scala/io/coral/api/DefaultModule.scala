e147cf0538a7fbc969739b14e55d16c3d3d9a03b
jaspervz <jvz@net-v.com>
2015-04-02 16:18:43 +0200

    Always include DefaultActorPropFactory

--- runtime-api/src/main/scala/io/coral/api/DefaultModule.scala
+++ runtime-api/src/main/scala/io/coral/api/DefaultModule.scala
@@ -4,14 +4,26 @@ import akka.actor.ActorSystem
 import io.coral.actors.ActorPropFactory
 import scaldi.Module
 import scala.collection.JavaConversions._
+import io.coral.actors.DefaultActorPropFactory
 
 class DefaultModule(implicit system: ActorSystem) extends Module {
+  private val ActorPropFactoriesConfigPath = "injections.actorPropFactories"
+
   for (actorPropFactory <- createActorPropFactories) {
     bind[ActorPropFactory] identifiedBy actorPropFactory._1 to actorPropFactory._2
   }
 
   private def createActorPropFactories: List[(String, ActorPropFactory)] = {
-    val classNames:List[String] = (system.settings.config getStringList "injections.actorPropFactories").toList
-    classNames.map(name => (name, Class.forName(name).newInstance.asInstanceOf[ActorPropFactory]))
+    getActorPropFactoryClassNames.map(name => (name, Class.forName(name).newInstance.asInstanceOf[ActorPropFactory]))
+  }
+
+  private def getActorPropFactoryClassNames: List[String] = {
+    val additionalClassNames = if (system.settings.config.hasPath(ActorPropFactoriesConfigPath)) {
+      (system.settings.config getStringList ActorPropFactoriesConfigPath).toList
+    } else {
+      List()
+    }
+
+    classOf[DefaultActorPropFactory].getName :: additionalClassNames
   }
 }
