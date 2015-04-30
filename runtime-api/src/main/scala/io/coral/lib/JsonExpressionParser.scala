dec23fd928bea67c08789866792fdf64cd107fe9
steven <stevenraemaekers@gmail.com>
2015-04-30 12:51:45 +0200

    Further implemented the CoralScriptActor. Added unit tests for separate script statements.

--- runtime-api/src/main/scala/io/coral/lib/JsonExpressionParser.scala
+++ runtime-api/src/main/scala/io/coral/lib/JsonExpressionParser.scala
@@ -19,75 +19,75 @@ case class ArrayAccess(id: JsonIdentifier, index: Int) extends FieldElement
 case class DictionaryAccess(id: JsonIdentifier, field: String) extends FieldElement
 
 object JsonExpressionParser extends JavaTokenParsers with PackratParsers {
-	/**
-	 * Parsers an expression string from a JSON field and
-	 * returns the JSON value that the expression points to.
-	 * @param expression The expression to parse.
-	 * @return The JObject that was referred to in the expression
-	 */
-	def parse(expression: String, json: JObject): JValue = {
-		val parseResult = phrase(local_field_reference)(
-			new PackratReader(new CharSequenceReader(expression)))
+  /**
+   * Parsers an expression string from a JSON field and
+   * returns the JSON value that the expression points to.
+   * @param expression The expression to parse.
+   * @return The JObject that was referred to in the expression
+   */
+  def parse(expression: String, json: JObject): JValue = {
+    val parseResult = phrase(local_field_reference)(
+      new PackratReader(new CharSequenceReader(expression)))
 
-		parseResult match {
-			case Success(r, n) =>
-				val value = r match {
-					case r: FieldReference => getFieldValue(json, r)
-				}
+    parseResult match {
+      case Success(r, n) =>
+        val value = r match {
+          case r: FieldReference => getFieldValue(json, r)
+        }
 
-				value.asInstanceOf[JValue]
-			case Failure(r, n) =>
-				println(parseResult)
-				JNothing
-		}
-	}
+        value.asInstanceOf[JValue]
+      case Failure(r, n) =>
+        println(parseResult)
+        JNothing
+    }
+  }
 
-	/**
-	 * Returns the value of a field that a FieldReference points to.
-	 * @param json The JSON object to extract the value from
-	 * @param id The FieldReference to extract
-	 * @return The JValue that the FieldReference points to
-	 */
-	def getFieldValue(json: JObject, id: FieldReference): JValue = {
-		// tempJson holds the result we want to return
-		var tempJson: JValue = json
+  /**
+   * Returns the value of a field that a FieldReference points to.
+   * @param json The JSON object to extract the value from
+   * @param id The FieldReference to extract
+   * @return The JValue that the FieldReference points to
+   */
+  def getFieldValue(json: JObject, id: FieldReference): JValue = {
+    // tempJson holds the result we want to return
+    var tempJson: JValue = json
 
-		id.items.foreach({
-			case i: JsonIdentifier =>
-				tempJson = tempJson \ i.id
-			case a: ArrayAccess =>
-				val obj = tempJson \ a.id.id
-				obj match {
-					case array: JArray =>
-						if (a.index < array.arr.length)
-							tempJson = array(a.index)
-						else return JNothing
-					case _ => return JNothing
-				}
-			case d: DictionaryAccess =>
-				tempJson = tempJson \ d.id.id \ d.field
-			case _ =>
-		})
+    id.items.foreach({
+      case i: JsonIdentifier =>
+        tempJson = tempJson \ i.id
+      case a: ArrayAccess =>
+        val obj = tempJson \ a.id.id
+        obj match {
+          case array: JArray =>
+            if (a.index < array.arr.length)
+              tempJson = array(a.index)
+            else return JNothing
+          case _ => return JNothing
+        }
+      case d: DictionaryAccess =>
+        tempJson = tempJson \ d.id.id \ d.field
+      case _ =>
+    })
 
-		tempJson
-	}
+    tempJson
+  }
 
-	type P[+T] = PackratParser[T]
+  type P[+T] = PackratParser[T]
 
-	lazy val local_field_reference: P[FieldReference] =
-		repsep(field_element, ".") ^^ { case i => FieldReference(i)}
-	lazy val field_element: P[FieldElement] =
-		array_access | dictionary_access | json_identifier
-	lazy val json_identifier: P[JsonIdentifier] =
-		ident ^^ { case i => JsonIdentifier(i) }
-	lazy val array_access: P[ArrayAccess] =
-		json_identifier ~ "[" ~ wholeNumber ~ "]" ^^ {
-			case id ~ "[" ~ index ~ "]" =>
-				ArrayAccess(id, index.toInt)
-		}
-	lazy val dictionary_access: P[DictionaryAccess] =
-		json_identifier ~ "[" ~ "'" ~ ident ~ "'" ~ "]" ^^ {
-			case id ~ "[" ~ "'" ~ field ~ "'" ~ "]" =>
-				DictionaryAccess(id, field)
-		}
+  lazy val local_field_reference: P[FieldReference] =
+    repsep(field_element, ".") ^^ { case i => FieldReference(i)}
+  lazy val field_element: P[FieldElement] =
+    array_access | dictionary_access | json_identifier
+  lazy val json_identifier: P[JsonIdentifier] =
+    ident ^^ { case i => JsonIdentifier(i) }
+  lazy val array_access: P[ArrayAccess] =
+    json_identifier ~ "[" ~ wholeNumber ~ "]" ^^ {
+      case id ~ "[" ~ index ~ "]" =>
+        ArrayAccess(id, index.toInt)
+    }
+  lazy val dictionary_access: P[DictionaryAccess] =
+    json_identifier ~ "[" ~ "'" ~ ident ~ "'" ~ "]" ^^ {
+      case id ~ "[" ~ "'" ~ field ~ "'" ~ "]" =>
+        DictionaryAccess(id, field)
+    }
 }
