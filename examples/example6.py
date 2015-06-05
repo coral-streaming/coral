fa6364dd613a372ffcac63ee32c37044198f5c3c
jaspervz <jvz@net-v.com>
2015-06-05 14:16:06 +0200

    more comments for example

--- examples/example6.py
+++ examples/example6.py
@@ -1,6 +1,6 @@
 from httpMethods import *
 
-# Reads from Kafka JSON in the format {"temperature": degrees, "date": "thedate"} and sends JSON back to Kafka if the temperature is 30 degrees or higher.
+# Reads from the Kafka topic source JSON in the format {"temperature": degrees, "date": "thedate"} and sends JSON back to the Kafka topic destination if the temperature is 30 degrees or higher.
 post('/api/actors', {"data": {"type": "actors", "attributes": {"type":"kafka-consumer", "params": {"topic": "source", "kafka": {"zookeeper.connect": "localhost:2181", "group.id": "uniqueid"}}}}})
 post('/api/actors', {"data": {"type": "actors", "attributes": {"type":"threshold", "params": {"key": "temperature", "threshold": 30}}}})
 post('/api/actors', {"data": {"type": "actors", "attributes": {"type":"json", "params": {"template": {"message": {"temperature": "${temperature}", "date": "${date}"}}}}}})
