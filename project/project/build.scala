3ab1acd6987bb6c586a29440cd080c2c9026346a
Natalino Busa <natalino.busa@gmail.com>
2015-07-22 13:44:53 +0200

    guards the sbt task for coveralls

--- project/project/build.scala
+++ project/project/build.scala
@@ -8,11 +8,18 @@ object ProjectBuild extends Build {
     scalacOptions := Seq("-deprecation", "-feature", "-encoding", "utf8")
   )
 
-  lazy val root = Project(
+  lazy val project = Project(
     id = "project",
     base = file("."),
-    settings = projectSettings).dependsOn(RootProject(coverallsPlugin))
+    settings = projectSettings)
 
-  lazy val coverallsPlugin = uri("git://github.com/coral-streaming/sbt-coveralls")
+  lazy val root = sys.props.get("coveralls") match {
+    case Some(_) =>
+      val coverallsPlugin = uri("git://github.com/coral-streaming/sbt-coveralls")
+      project.dependsOn(RootProject(coverallsPlugin))
+    case _ =>
+      project
+  }
+
 }
 
